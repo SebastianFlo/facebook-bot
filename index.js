b0a1e9e1f7db6ba8f98b0b2d00dfd1e2dfe325b0
Sebastian Florian <rebardesigndk@gmail.com>
2016-04-23 17:13:56 +0200

    feature(index): Added send and receive

--- index.js
+++ index.js
@@ -20,6 +20,43 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token');
 })
 
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
+
+var token = "EAACsMTTvGyUBACuG9QTMlEd1w0BkJd60E12l7jbHmBsD6ZCEuUIxa3ErbUKktZB8ZCQHUq1MzbYXWl21ZB2ZArgZCr5W9MMGbtY7JT8bhnLUvyETqUOBZCAHkD6zFlDjVqKn4ZCT3Pf1VMHe1V8EGt9x57atQk31NIQZBA20IAvh0nQZDZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
