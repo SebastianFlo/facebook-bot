3995177e32fb5ac9c449bdc0e4baf292f82d63df
Sebastian Florian <rebardesigndk@gmail.com>
2016-04-24 17:33:39 +0200

    chore(index): Add conf token

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 var key = require('./confidential/token.js');
-console.log(key.token.page);
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -122,5 +122,3 @@ function sendGenericMessage(sender) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
