ef9344046c5279e0873c9851a45a463fe2f917e7
Sebastian Florian <rebardesigndk@gmail.com>
2016-04-24 16:33:51 +0200

    added token key

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
-
+var key = require('./confidential/token.js');
+console.log(key.token.page);
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
